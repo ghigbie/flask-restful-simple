5e98f1f130550bb57dd4832a8ce652edd343a5da
ghigbie <gehigbie@gmail.com>
2020-06-07 21:44:38 -0700

    Edit url

--- app/app.py
+++ app/app.py
@@ -1,11 +1,13 @@
-from flask import flask
+from flask import Flask
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
 
-class Student(Rsource):
+class Student(Resource):
     def get(self, name):
         return {'student': name}
 
-api.add_resource(Student, '/student/<string: name>')
+api.add_resource(Student, '/student/<string:name>')
+
+app.run(port=5000)
